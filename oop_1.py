8dc6c302dc41bc7ec584b640c8825c11f2d9bfb6
HASNAT <61647150+hasnatosman@users.noreply.github.com>
2022-11-26 11:02:53 +0600

    Add files via upload

---
+++ oop_1.py
@@ -0,0 +1,18 @@
+# creating class.......................
+
+class Employee:
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+
+emp_1 = Employee('hasnat', 'osman', 50000)
+emp_2 = Employee('user', 'test', 60000)
+
+print(emp_1.fullname())
+print(emp_2.fullname())
